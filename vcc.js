c948a7999eac010cfd8477f51634ca30fea4a53a
dandavis <rndme@users.noreply.github.com>
2016-02-07 19:11:31 -0600

    remove VCC.DOM

--- vcc.js
+++ vcc.js
@@ -163,36 +163,8 @@ function VCC(def) {
 	  	if(v===true) return k+" ";
 	  	return k+"="+JSON.stringify(v);
 	}).join("");
-  }
+  };
   
-VCC.DOM={
- _: function(tagName, attribs, content){
-	var elm=document.createElement(tagName);
-	Object.keys(attribs||{}).forEach(function(k){
-		if(attribs[k]!==false) elm.setAttribute(k, attribs[k]);
-	});
-	if(content){
-		if(typeof content==="string"){
-			elm.innerHTML=content;
-		}else{
-			elm.appendChild(content);
-		}
-	}
-	return elm.outerHTML;
- }
-};
-
-"a,abbr,acronym,address,applet,area,article,aside,audio,b,base,basefont,bdi,bdo,big,blockquote,body,br,button,canvas,caption,center,cite,code,col,colgroup,datalist,dd,del,details,dfn,dialog,dir,div,dl,dt,em,embed,fieldset,figcaption,figure,font,footer,form,frame,frameset,h1,h2,h3,h4,h5,h6,head,header,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,main,map,mark,menu,menuitem,meta,meter,nav,noframes,noscript,object,ol,optgroup,option,output,p,param,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,small,source,span,strike,strong,style,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,var,video,wbr".split(",").forEach(function(k){
-   VCC.DOM[k]=VCC.DOM._.bind(null, k);
-});
-
-/*
-var exampleList= VCC.DOM.ul({ className: 'my-list' },
-     VCC.DOM.li(null, 'Text Content')
-);
-*/
-
-
 return VCC;
 }));
   
